b9309f30a00122b8f0faeaa584a1e0e7d10b845b
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2017-03-01 14:51:37 -0300

    Added release 1

---
+++ README.md
@@ -0,0 +1,23 @@
+# Clear logs
+
+A lib to provide more context around logs, making those clear and easy to display
+
+## 1. Install & import
+
+`npm install clear_logs`
+
+```js
+const logger = require('clear_logs');
+```
+
+## 2. Just use it
+```js
+logger.log( 'Something', 'something 2', 'something 3');
+```
+
+Where the methods are:
+**log**
+**eror**
+**info**
+**warn**
+**debug**
---
+++ logger.js
@@ -0,0 +1,69 @@
+
+/**
+* Setup loggin
+*/
+
+require('padleft');
+
+const timestamp = require('./lib/timestamp');
+const tracer = require('./lib/tracer');
+const serializer = require('./lib/serializer');
+const extend = require('util')._extend;
+
+const bsh = {
+  error: '\033[1;31m', // red & bold
+  info: '\033[1;34m', // blue & bold
+  warn: '\033[1;33m', // yellow & bold
+  log: '\033[1;32m', // green & bold
+  debug: '\033[1;36m', // cyan & bold
+  appendix: '\033[0;37m', // light gray
+  reset: '\033[0m',
+  arrow: '\033[1;90m', // dark grey
+  intro: '\033[0;95m', // light purple
+  line: '\033[0;94m' // light blue
+};
+
+const config = {
+  appendix: null,
+  stackUp: 11,
+  envs: {
+    production: [ 'error', 'log', 'warn' ],
+    staging: [ 'error', 'log', 'warn' ],
+    test: [ 'error', 'log', 'warn', 'debug', 'info' ],
+    development: [ 'error', 'log', 'warn', 'debug', 'info' ]
+  }
+}
+
+function log( ) {
+  
+  var envCfg = config.envs[process.env.NODE_ENV || 'development'];
+  var [ method, ...args ] = Array.from( arguments );
+  
+  if ( !envCfg.includes( method ) ) { return; }
+  
+  var prefix = `${bsh.intro}${timestamp.now()}${bsh.reset} ` + 
+    `${bsh[method]}${method.toUpperCase()}${bsh.reset} ` + 
+    `${bsh.line}${tracer.lineInfo( config.stackUp )}${bsh.reset} ` +
+    ( config.appendix ? `${bsh.appendix}[${config.appendix}]${bsh.reset} ` : '') + 
+    `${bsh.arrow}>>${bsh.reset}`;
+  
+  return console.log( prefix, ...args.map( serializer.stringifyInfo ) );
+}
+
+module.exports = {
+  
+  /**
+   * Configure logger
+   * @param config <Object> | Setup the settings for this logger
+   */
+  config( _config ) { extend( config, _config ); },
+  
+  /**
+   * Logging methods
+   */
+  debug( ) { log( 'debug', ...Array.from(arguments) ); },
+  log( ) { log( 'log', ...Array.from(arguments) ); },
+  error( ) { log( 'error', ...Array.from(arguments) ); },
+  warn( ) { log( 'warn', ...Array.from(arguments) ); },
+  info( ) { log( 'info', ...Array.from(arguments) ); }
+};
